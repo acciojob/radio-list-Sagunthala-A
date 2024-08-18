f146a26fa4c9247196bb8017417c537b9c6f8052
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-08-18 08:31:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-//your JS code here. If required.
-
+let div = document.createElement('div');
+document.body.append(div)
 
 for(let i = 0; i<6; i++){
 	let radio = document.createElement('input');
 	let label = document.createElement('label');
 	let br = document.createElement('br');
-	label.innerText = `Option ${i+1}`
+	label.innerText = `Option ${i+1}`;
 	
 	radio.type ='radio';
+	radio.name= 'ans';
+	if((i+1)%2 == 0){
+		radio.setAttribute('class','input')
+	}
+	if(i == 1){
+		radio.disabled = true;
+	}
 	
-	document.body.append(label,radio,br);
+	div.append(radio,label,br);
 }
