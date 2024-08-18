6b356bf4865d465d4ebca01252a3b35cb999cdc5
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-08-18 08:17:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+
+for(let i = 0; i<6; i++){
+	let radio = document.createElement('input');
+	let label = document.createElement('label');
+	let br = document.createElement('br');
+	label.innerText = `Option ${i+1}`
+	
+	radio.type ='radio';
+	
+	document.body.append(label,radio,br);
+}
